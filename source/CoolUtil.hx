08b1f75be68ccf7523554f588dcc6fe2078f1e63
TorchTheDragon <72819066+TorchTheDragon@users.noreply.github.com>
2023-05-10 03:23:59 -0400

    fixed tabs

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -91,16 +91,16 @@ class CoolUtil
 		var countByColor:Map<Int, Int> = [];
 		for(col in 0...sprite.frameWidth){
 			for(row in 0...sprite.frameHeight){
-			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-			  if(colorOfThisPixel != 0){
-				  if(countByColor.exists(colorOfThisPixel)){
-				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
-				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
-					 countByColor[colorOfThisPixel] = 1;
-				  }
-			  }
+				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+				if(colorOfThisPixel != 0){
+					if(countByColor.exists(colorOfThisPixel)){
+						countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
+					}else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
+						countByColor[colorOfThisPixel] = 1;
+					}
+				}
 			}
-		 }
+		}
 		var maxCount = 0;
 		var maxKey:Int = 0;//after the loop this will store the max color
 		countByColor[flixel.util.FlxColor.BLACK] = 0;
